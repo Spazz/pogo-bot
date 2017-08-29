a10bd3fada8ac063ae8f8cdd92f3a0bd7cc7b17e
Spazz <brandon_l121@yahoo.com>
2017-08-28 20:29:06 -0500

    Added suffix ability to .rolecount

--- app.js
+++ app.js
@@ -96,20 +96,38 @@ var commands = {
         public: true,
         description: "User count of each Role.",
         process: function(bot, msg, suffix) {
-            
-            var roles = msg.guild.roles;
-
             var txt = ""
-            roles.forEach(function(val, ind) {
+            var roles = msg.guild.roles;
+            
+            if(suffix) {
+
+                var role = suffix;
+                role = msg.guild.roles.find('name', suffix);
+                var roleMembers = role.members;
                 
-                if(msg.member.hasPermission("MANAGE_ROLES", false, true, true)) {
-                    txt += val.name + " - " + val.members.size + "\n";
-                } else {
-                    if(val.name === "Mystic" || val.name === "Valor" || val.name === "Instinct") {
-                        txt += val.name + " - " + val.members.size + "\n";
-                    }
+                txt = 'Members with ' + suffix + ' role. \n';
+
+                roleMembers.forEach(function(member) {
+                    txt += member.user.username + '\n';
+                });
+
+                if(txt.length > 1999) {
+                    txt = "This list is longer than a single discord message can send."
                 }
-            });
+            } else {
+
+                roles.forEach(function(val, ind) {
+                    
+                    if(msg.member.hasPermission("MANAGE_ROLES", false, true, true)) {
+                        txt += val.name + " - " + val.members.size + "\n";
+                    } else {
+                        if(val.name === "Mystic" || val.name === "Valor" || val.name === "Instinct") {
+                            txt += val.name + " - " + val.members.size + "\n";
+                        }
+                    }
+                });
+            }
+
             msg.author.send(txt);
         }
     },
